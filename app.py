8d730809cdac5bfcb41d310731ecac0eee8a514f
Vasiliy Sharapov <vasiliys@gmail.com>
2023-10-13 22:02:37 -0400

    Barebones webapp for one message chat

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
+#!/usr/bin/env python
+
 from flask import Flask, render_template, request, jsonify
-import subprocess
+import openai
+import load_env
+import os
+
+# Load environment variables from all .env files in the current directory
+load_env.load()
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/run_script', methods=['POST'])
-def run_script():
-    proc = subprocess.Popen(["./venv/bin/python", "main.py"], stdout=subprocess.PIPE)
-    output = proc.stdout.read().decode('utf-8')
-    return jsonify({'output': output})
+@app.route('/openai', methods=['POST'])
+def openai_post():
+    data = request.json
+    from pprint import pprint as pp
+    pp(data)
+    endpoint = data["openaiEndpoint"]
+    model = data["model"]
+    messages = [
+        {"role": data["role1"], "content": data["content1"]},
+        {"role": data["role2"], "content": data["content2"]}
+    ]
+    
+    response = getattr(openai, endpoint).create(model=model, messages=messages)
+    pp(response)
+    return jsonify({'output': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
